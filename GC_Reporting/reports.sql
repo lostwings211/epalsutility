24bc53800e370a89da2f6a8e44e4f776c83ec03b
Zhili Hua <zhua@corp.epals.com>
2014-05-12 16:10:52 -0400

    modify reports.sql to let it dynamically load up the cuteoff date

--- GC_Reporting/reports.sql
+++ GC_Reporting/reports.sql
@@ -1,3 +1,5 @@
-select count(*) as 'GC Users' from users where date_added <= '2014-04-01 00:00:00' and reg_cobrand <=9999;
+SET @cutoffdate = '2014-01-01 00:00:00';
 
-select count(*) as 'SM Legacy' from users where date_added <= '2014-04-01 00:00:00' and reg_cobrand > 9999;
+select count(*) as 'GC Users' from users where date_added <= @cutoffdate and reg_cobrand <=9999;
+
+select count(*) as 'SM Legacy' from users where date_added <= @cutoffdate and reg_cobrand > 9999;
