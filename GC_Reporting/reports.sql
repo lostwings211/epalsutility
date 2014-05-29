6ab3713317ad182650b6f68ba64f3fb893569928
Zhili Hua <zhua@corp.epals.com>
2014-05-29 11:12:04 -0400

    take into consideration of the Legacy SM users that have been archived

--- GC_Reporting/reports.sql
+++ GC_Reporting/reports.sql
@@ -1,5 +1,6 @@
-SET @cutoffdate = '2014-01-01 00:00:00';
+SET @cutoffdate = '2014-05-01';
+SET @startarchivedate = '2014-04-01';
 
 select count(*) as 'GC Users' from users where date_added <= @cutoffdate and reg_cobrand <=9999;
 
-select count(*) as 'SM Legacy' from users where date_added <= @cutoffdate and reg_cobrand > 9999;
+select (select count(*) from users where date_added <= @cutoffdate and reg_cobrand > 9999) + (select count(*) from archivedUsers where archiveTime > @startarchivedate) as 'SM Legacy';
